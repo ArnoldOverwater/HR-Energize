49316b8f8cc57142db2239b7d5f1d5fe5a35d047
ArnoldOverwater <arnoldoverwater@gmail.com>
2012-03-11 21:19:51 +0100

    First commit of default created project in eclipse

---
+++ HR-Energize/src/nl/hr/energize/HR_EnergizeServlet.java
@@ -0,0 +1,13 @@
+package nl.hr.energize;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HR_EnergizeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
